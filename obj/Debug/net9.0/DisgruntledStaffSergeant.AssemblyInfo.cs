d4053f254651afc9fec7fabfe00dec6e44fa9295
Michael D. Connell Jr. <85039481+Spaun12@users.noreply.github.com>
2024-12-23 18:56:22 -0600

    Making sure it works

--- obj/Debug/net9.0/DisgruntledStaffSergeant.AssemblyInfo.cs
+++ obj/Debug/net9.0/DisgruntledStaffSergeant.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DisgruntledStaffSergeant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1a54e4972c6c5f4c6a40c7787a98324d01da0ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("DisgruntledStaffSergeant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DisgruntledStaffSergeant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
